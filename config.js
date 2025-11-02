169e5d5229afe69056b424e51c9efb1e8a32fb2f
anasjaveed19 <anasjaveed739@gmail.com>
2025-11-02 13:41:38 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,41 @@
+// anasjaveed19 2019.10.10 https://github.com/AJLoveChina/birthday
+var config = {
+    // The length of the sentence can be arbitrary. You can write ten or twenty sentences.
+    // Try to keep each sentence under 15 words, otherwise, the display effect may not be good.
+    texts: [
+        "For my amazing friend,",
+        "Shahroz Ghaffar!",
+        "Today is your special day!",
+        "May your smile shine bright,",
+        "and your dreams come true.",
+        "Wishing you endless happiness,",
+        "success, and peace ahead.",
+        "You are truly one of a kind!",
+        "Enjoy your birthday celebration!",
+        "🎉 Happy Birthday Shahroz Ghaffar! 🎂",
+    ],
+    /**
+     * imgs can be left blank, but if you want to fill it in, you must follow the format below
+     * "Corresponding text above, must be exactly the same": "Image address, images can be placed in the imgs folder"
+     * For example
+     * "Shahroz Ghaffar!": "./imgs/shahroz.jpg"
+     *
+     * If you don't want pictures, just write two slashes at the beginning of each line to comment it out.
+     * Tip: It's best to use square or near-square pictures for a better look.
+     */
+    imgs: {
+         "Shahroz Ghaffar!": "./imgs/xiaokeai.png",
+        // "Today is your special day!": "./imgs/birthday.jpg",
+    },
+    // Button text descriptions. The following are the default button texts, you can change them to whatever you like.
+    desc: {
+        turn_on: "Start",
+        play: "Music",
+        bannar_coming: "Colors",
+        balloons_flying: "Something's missing",
+        cake_fadein: "Cake?",
+        light_candle: "Candle?",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
